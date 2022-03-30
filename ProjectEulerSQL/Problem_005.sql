e7fe5c1b5fba300676f918f410a076cb33f136a8
Gray <33128934+GreyOrGray@users.noreply.github.com>
2022-03-30 14:20:54 -0600

    Update Problem_005.sql

--- ProjectEulerSQL/Problem_005.sql
+++ ProjectEulerSQL/Problem_005.sql
@@ -5,6 +5,8 @@ Problem 5
 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+Fiddle at http://sqlfiddle.com/#!18/9eecb/159128
 ************************************************************************************************************************/
 
 --1 brute force
